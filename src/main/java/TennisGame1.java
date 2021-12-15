c41d67064b5246bb6284374f9c9ab0945e896816
Lukas-jb <92315994+Lukas-jb@users.noreply.github.com>
2021-12-15 14:38:15 -0500

    Refactorizacion 1
    Se mórfica nombre de los puntajes
    Se establecen los nombres como final los nombres de los jugadores
    Se Refactoriza el metodo getScore() incorporando una evaluación equalScoreString()

--- src/main/java/TennisGame1.java
+++ src/main/java/TennisGame1.java
@@ -1,76 +1,78 @@
 
 public class TennisGame1 implements TennisGame {
-    
-    private int m_score1 = 0;
-    private int m_score2 = 0;
-    private String player1Name;
-    private String player2Name;
+
+    private int scorePlayer1 = 0;
+    private int scorePlayer2 = 0;
+    private final String PLAYER1NAME; //variables final en mayusculas mejoran su reconocimiento
+    private final String PLAYER2NAME;
 
     public TennisGame1(String player1Name, String player2Name) {
-        this.player1Name = player1Name;
-        this.player2Name = player2Name;
+        this.PLAYER1NAME = player1Name;
+        this.PLAYER2NAME = player2Name;
     }
 
     public void wonPoint(String playerName) {
-        if (playerName == "player1")
-            m_score1 += 1;
-        else
-            m_score2 += 1;
+        if (playerName == "player1") scorePlayer1 += 1;
+        else scorePlayer2 += 1;
     }
 
     public String getScore() {
         String score = "";
-        int tempScore=0;
-        if (m_score1==m_score2)
-        {
-            switch (m_score1)
-            {
-                case 0:
-                        score = "Love-All";
-                    break;
-                case 1:
-                        score = "Fifteen-All";
-                    break;
-                case 2:
-                        score = "Thirty-All";
-                    break;
-                default:
-                        score = "Deuce";
-                    break;
-                
-            }
-        }
-        else if (m_score1>=4 || m_score2>=4)
-        {
-            int minusResult = m_score1-m_score2;
-            if (minusResult==1) score ="Advantage player1";
-            else if (minusResult ==-1) score ="Advantage player2";
-            else if (minusResult>=2) score = "Win for player1";
-            else score ="Win for player2";
-        }
-        else
-        {
-            for (int i=1; i<3; i++)
-            {
-                if (i==1) tempScore = m_score1;
-                else { score+="-"; tempScore = m_score2;}
-                switch(tempScore)
-                {
+        int tempScore = 0;
+        if (scorePlayer1 == scorePlayer2) {
+            score = equalScoreString();
+
+        } else if (scorePlayer1 >= 4 || scorePlayer2 >= 4) {
+            int minusResult = scorePlayer1 - scorePlayer2;
+            if (minusResult == 1) score = "Advantage player1";
+            else if (minusResult == -1) score = "Advantage player2";
+            else if (minusResult >= 2) score = "Win for player1";
+            else score = "Win for player2";
+        } else {
+            for (int i = 1; i < 3; i++) {
+                if (i == 1) tempScore = scorePlayer1;
+                else {
+                    score += "-";
+                    tempScore = scorePlayer2;
+                }
+                switch (tempScore) {
                     case 0:
-                        score+="Love";
+                        score += "Love";
                         break;
                     case 1:
-                        score+="Fifteen";
+                        score += "Fifteen";
                         break;
                     case 2:
-                        score+="Thirty";
+                        score += "Thirty";
                         break;
                     case 3:
-                        score+="Forty";
+                        score += "Forty";
                         break;
                 }
             }
         }
         return score;
     }
+
+    private String equalScoreString() {
+        String score;
+        switch (scorePlayer1) {
+            case 0:
+                score = "Love-All";
+                break;
+            case 1:
+                score = "Fifteen-All";
+                break;
+            case 2:
+                score = "Thirty-All";
+                break;
+            default:
+                score = "Deuce";
+                break;
+
+        }
+        return score;
+    }
+
 }
+
